3e1113cb4d9b395bec728efc164f73ca2faa6a86
LuanLucasGon <lucasluangon@gmail.com>
2024-12-17 16:19:08 -0300

    feat: User entity

---
+++ src/main/java/com/lucasluan/springMongo/domain/User.java
@@ -0,0 +1,36 @@
+package com.lucasluan.springMongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
