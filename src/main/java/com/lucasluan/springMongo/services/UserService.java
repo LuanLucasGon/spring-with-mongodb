f5a9011b35243872a7c7dcdd37ad5b620d5084e9
LuanLucasGon <lucasluangon@gmail.com>
2024-12-18 10:40:05 -0300

    feat: Implementing user findById

--- src/main/java/com/lucasluan/springMongo/services/UserService.java
+++ src/main/java/com/lucasluan/springMongo/services/UserService.java
@@ -2,10 +2,13 @@ package com.lucasluan.springMongo.services;
 
 import com.lucasluan.springMongo.domain.User;
 import com.lucasluan.springMongo.repositories.UserRepository;
+import com.lucasluan.springMongo.services.exceptions.ObjectNotFoundException;
+import com.sun.jdi.ObjectCollectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +18,10 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
